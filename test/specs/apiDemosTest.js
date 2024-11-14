903a79bf2c3c2e04d96fd48b4b289187f073332f
guilhermebilck <ghbilck@gmail.com>
2024-11-14 14:58:29 -0300

    Tarefa 2 e resultados

--- test/specs/apiDemosTest.js
+++ test/specs/apiDemosTest.js
@@ -13,4 +13,32 @@ describe('ApiDemos App Test', function () {
         const newPageText = await $('android=new UiSelector().textContains("Enable TalkBack and Explore-by-touch from accessibility settings")').getText();
         expect(newPageText).toContain('Enable TalkBack and Explore-by-touch from accessibility settings');
     });
-});
+
+    it('should fill out an example form', async () => {
+        await driver.startActivity('io.appium.android.apis', '.ApiDemos');
+
+        await driver.pause(5000);
+
+        const accessibilityButton = await $('android=new UiSelector().text("OS")');
+        await accessibilityButton.click();
+
+        const accessibilityNodeProviderButton = await $('android=new UiSelector().text("SMS Messaging")');
+        await accessibilityNodeProviderButton.click();
+
+        const recipientField = await $('id:io.appium.android.apis:id/sms_recipient');
+        if (recipientField.isDisplayed()) {
+            await recipientField.setValue('5548996320902');
+        }
+
+        const contentField = await $('id:io.appium.android.apis:id/sms_content');
+        if (contentField.isDisplayed()) {
+            await contentField.setValue('Envio de dados de formulário');
+        }
+
+        const sendButton = await $('id:io.appium.android.apis:id/sms_send_message');
+        if (sendButton.isDisplayed()) {
+            await sendButton.click();
+        }
+    });
+
+})
